f31f7a2740ac819c18088177c3969e56e709549f
Shawn Zhang <pgz11902@gmail.com>
2018-11-29 23:30:48 -0500

    bg.txt, pos and neg

--- rotate_and_flip.py
+++ rotate_and_flip.py
@@ -9,34 +9,36 @@ count = 0
 FILENAME = 'pos'
 
 def process(pic,output):
+	global count
 
 	img = cv2.imread(pic)
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	for i in range(30):
 		count+=1
-		cv2.imwrite(output + '/' + FILENAME + str(count),img)
+		cv2.imwrite(output + '/' + FILENAME + str(count) + '.png',img)
 
 		count+=1
-		flip0 = cv2.flip(img,1)
-		cv2.imwrite(output + '/' + FILENAME + str(count),flip0)
+		flip0 = cv2.flip(img,0)
+		cv2.imwrite(output + '/' + FILENAME + str(count) + '.png',flip0)
 
 		count+=1
 		flip1 = cv2.flip(img,1)
-		cv2.imwrite(output + '/' + FILENAME + str(count),flip1)
+		cv2.imwrite(output + '/' + FILENAME + str(count) + '.png',flip1)
 
 		(rows,cols) = img.shape[:2]
 
-		M = cv2.getRotationMatrix(	(cols/2,rows/2),random.randInt(8,15),1)
+		M = cv2.getRotationMatrix2D(	(cols/2,rows/2),random.randint(8,15),1)
 		img = cv2.warpAffine(img,M,(cols,rows))
 
+	print(count)
 
 
 if __name__ == "__main__":
 	inputfolder = sys.argv[1]
 	outputfolder = sys.argv[2]
 
-	in = glob.glob('%s/*.png' % inputfolder)
+	inp = glob.glob('%s/*.png' % inputfolder)
 
-	for innie in in:
+	for innie in inp:
 		process(innie,outputfolder)
